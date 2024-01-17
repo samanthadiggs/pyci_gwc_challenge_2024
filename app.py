f293a538d4aeea6c70e4678daa434465a8db5a5e
samantha diggs <99908108+samanthadiggs@users.noreply.github.com>
2024-01-17 11:08:10 -0500

    adding backend

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+
+#flask constructor
+app = Flask(__name__)
+
+#root endpoint
+@app.route('/', methods=['GET']) 
+def index(): 
+    ## Display the HTML form template  
+    return render_template('signup.html') 
+
+# `read-form` endpoint  
+@app.route('/read-form', methods=['POST']) 
+def read_form(): 
+  
+    # Get the form data as Python ImmutableDict datatype  
+    data = request.form 
+  
+    ## Return the extracted information  
+    return { 
+        'emailId'     : data['email'], 
+        'phoneNumber' : data['phoneNumber'], 
+        'password'    : data['password'], 
+        'firstName' : data['firstName'],
+        'lastName': data['lastName'],
+        'confirmPassword': data['confirmPassword'],
+        'T&C':data['terms'],
+
+    } 
+  
+# Main Driver Function 
+if __name__ == '__main__': 
+    # Run the application on the local development server 
+    app.run()
